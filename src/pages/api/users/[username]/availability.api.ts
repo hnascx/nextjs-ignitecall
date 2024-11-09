c915b2236a2681d87c2ff6b5e82d0c585f8bd675
hnascx <henriquehusky@gmail.com>
2024-11-08 23:12:24 -0300

    Blocking schedules in the past

--- src/pages/api/users/[username]/availability.api.ts
+++ src/pages/api/users/[username]/availability.api.ts
@@ -56,7 +56,7 @@ export default async function handle(
     },
   )
 
-  // greater than or equal
+  // gte => greater than or equal
 
   const blockedTimes = await prisma.scheduling.findMany({
     select: {
@@ -72,9 +72,13 @@ export default async function handle(
   })
 
   const availableTimes = possibleTimes.filter((time) => {
-    return !blockedTimes.some(
+    const isTimeBlocked = blockedTimes.some(
       (blockedTime) => blockedTime.date.getHours() === time,
     )
+
+    const isTimeInPast = referenceDate.set('hour', time).isBefore(new Date())
+
+    return !isTimeBlocked && !isTimeInPast
   })
 
   return res.json({ possibleTimes, availableTimes })
